0a87f441b78ccf13a5987f3b52fbb9abef7b4527
Nando-2002 <prthik.kar@gmail.com>
2025-12-16 22:02:31 +0100

    added neptune and uranus test

--- src/gpu_pork.py
+++ src/gpu_pork.py
@@ -58,6 +58,7 @@ def gpu_porkchop_test():
     # heliocentric
     mu = astroConstants(4)
 
+    ''' Neptune Case 
     # departure date range : 2 April 2003 to 1 August 2003
     departure_start = np.array([2025, 1, 1, 0, 0, 0])
     departure_end   = np.array([2026, 1, 1, 0, 0, 0])
@@ -65,6 +66,14 @@ def gpu_porkchop_test():
     # arrival date range : 1 Sept 2003 to 1 March 2004
     arrival_start = np.array([2036, 1, 1, 0, 0, 0])
     arrival_end   = np.array([2055, 6, 1, 0, 0, 0])
+    '''
+    # departure date range : 2 April 2003 to 1 August 2003
+    departure_start = np.array([2027, 1, 1, 0, 0, 0])
+    departure_end   = np.array([2029, 1, 1, 0, 0, 0])
+
+    # arrival date range : 1 Sept 2003 to 1 March 2004
+    arrival_start = np.array([2031, 4, 1, 0, 0, 0])
+    arrival_end   = np.array([2045, 12, 1, 0, 0, 0])
 
     # convert to mjd2000
     departure_start_mjd = date2mjd2000(departure_start)
@@ -93,7 +102,7 @@ def gpu_porkchop_test():
         ve_earth[i] = np.asarray(v).flatten()
 
         # Mars at arrival
-        aa, ea, ia, Oma, oma, thetaa = uplanet(arr_range[i], 8)
+        aa, ea, ia, Oma, oma, thetaa = uplanet(arr_range[i], 7)
         ha = np.sqrt(mu * aa * (1 - ea**2))
         r2, v2 = kep2car(ha, ea, ia, Oma, oma, thetaa, mu)
         rm_mars[i] = np.asarray(r2).flatten()
