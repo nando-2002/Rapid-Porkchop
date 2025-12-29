0cb5402610d8b57f6713aa0cb94b1f2f4883ceaf
Nando-2002 <prthik.kar@gmail.com>
2025-12-29 10:57:48 +0100

    revert

--- src/startFlyby.py
+++ src/startFlyby.py
@@ -6,11 +6,3 @@ soln, dep, fly, arr, r, v = call_flyby(50, 3, 4, 5)  # Earth->Mars flyby->Jupite
 results = (soln, dep, fly, arr)
 
 save_flyby_plot(results, "Earth_MarsFlyby_Jupiter.png")
-
-save_flyby_isosurface(results, filename="Earth_MarsFlyby_Jupiter_isosurface.png")
-
-# Or specify an explicit isovalue (ΔV in km/s) and threshold
-save_flyby_isosurface(results,
-                      filename="Earth_MarsFlyby_Jupiter_isosurface2.png",
-                      level=15.0,             # isosurface at ΔV = 15 km/s
-                      min_dv_threshold=30)    # mask values > 30 km/s as invalid
